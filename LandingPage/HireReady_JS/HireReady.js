2a8a34bee009d2a20d39d769d0e62c800fb05555
KarinaGarcia-M <u202323952@upc.edu.pe>
2025-11-10 00:50:01 -0500

    Update HireReady.js

--- LandingPage/HireReady_JS/HireReady.js
+++ LandingPage/HireReady_JS/HireReady.js
@@ -12,5 +12,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
             const headerOffset = 80;
+      const headerOffset = 80;
+            const elementPosition = target.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
 // parte 2
