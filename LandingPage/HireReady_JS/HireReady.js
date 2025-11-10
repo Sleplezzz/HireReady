8fdcc6bdcd60fc585d0269cb56b654fb34a9143f
KarinaGarcia-M <u202323952@upc.edu.pe>
2025-11-10 00:55:13 -0500

    Update HireReady.js

--- LandingPage/HireReady_JS/HireReady.js
+++ LandingPage/HireReady_JS/HireReady.js
@@ -2,7 +2,9 @@
 const menuToggle = document.getElementById('menuToggle');
 const navMenu = document.getElementById('navMenu');
 
-
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
 // Smooth scroll para navegación
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
